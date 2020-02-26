6646406a9474ef2603851d9414563dce62da181a
bartosz-bicki <bartosz.bicki.stud@gmail.com>
2020-02-26 18:00:50 +0100

    Poprawiona wersja ostateczna, spełniająca wszystkie założenia. Dodano komentarze oraz kod realizujący zadanie.

--- venv/main.py
+++ venv/main.py
@@ -1,38 +1,49 @@
 import sys
 import logging
-#__name__ statement provides the info about where the script was run
+
+# __name__ statement provides the info about where the script was run
+from os import path
+
 my_logger = logging.getLogger(__name__)
-#settig level to debug to get an information upon the console
+
+# settig level to debug to get an information upon the console
 my_logger.setLevel(logging.INFO)
 
-basic_formatter = logging.Formatter('%(name)s:%(asctime)s:%(message)s')
+# setting a simple formatter to specify given info
+basic_formatter = logging.Formatter("%(name)s:%(asctime)s:%(message)s")
 
-my_handler = logging.FileHandler('info.log')
+my_handler = logging.FileHandler("info.log")
 my_handler.setFormatter(basic_formatter)
 my_logger.addHandler(my_handler)
 
-#with this line script will not mistake our logger with default
+# with this line script will not mistake our logger with default
 logging.basicConfig(level=logging.DEBUG)
 
-def opening_file(arg):
 
-    #if it's first launch file will be created
-    file = open("words.txt",'a+',encoding='utf-8')
-    my_logger.info('File Open')
-
-    #as arguments comes as a list loop provides them to be separated
-    for i in arg:
-
-        if i not in file:
-            file.write(i + '\n')
-            my_logger.info('Adding arguments to the file')
-        else:
+def opening_file(args):
+    # I check if the file exists
+    if not path.exists("words.txt"):
+        with open("words.txt", "w"):
             pass
+        my_logger.info("File doesn't exists. Created new file.")
 
-    file.close()
-    my_logger.info('File has been closed')
+    # if file exists I open the file with decorator
+    with open("words.txt", "r+") as file:
+        my_logger.info("File Open.")
+        # reading given arguments to a variable
+        data = file.read()
+        # as arguments comes as a list loop provides them to be separated
+        for arg in args:
+            if arg not in data:
+                file.write(arg + "\n")
+                my_logger.info(f"Adding argument '{arg}' to the file.")
+        my_logger.info("File has been closed.")
 
-#[1:] statement assures that sccipt name won't be delivered as an argument
-arg = sys.argv[1:]
-my_logger.info('Receiving arguments')
-opening_file(arg)
+
+# [1:] statement assures that script name won't be delivered as an argument
+args = sys.argv[1:]
+unique_args = []
+# I check if the new arguments aren't double
+[unique_args.append(arg) for arg in args if arg not in unique_args]
+my_logger.info("Receiving arguments")
+opening_file(unique_args)
